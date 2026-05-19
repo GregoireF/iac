19c9bf6c43593b6a8d02352da1f2bf168554b47b
Gregoire <mohinette@hotmail.fr>
2026-05-19 02:15:29 +0200

    feat(github): enable allow_auto_merge on addlicense repo
    Signed-off-by: Gregoire <mohinette@hotmail.fr>

--- terraform/github/locals.tf
+++ terraform/github/locals.tf
@@ -88,6 +88,7 @@ locals {
       allow_merge_commit     = false
       allow_squash_merge     = true
       allow_rebase_merge     = false
+      allow_auto_merge       = true
       delete_branch_on_merge = true
       archived               = false
 
